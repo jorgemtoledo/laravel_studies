aaee8f91e0ceab48f868b786e1ee79d37beeaa89
jorgemtoledo <jorge.toledo@gmail.com>
2019-04-03 10:23:16 -0400

    Login Admin an Users

---
+++ loginAvancado/app/Http/Controllers/Auth/AdminLoginController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class AdminLoginController extends Controller
+{
+
+  public function __construct()
+  {
+    $this->middleware('guest:admin');
+  }
+
+
+  public function login(Request $request){
+    // return "OK";
+
+    $this->validate($request, [
+      'email' => 'required|string',
+      'password' => 'required|string',
+    ]);
+
+    $credentials = [
+      'email' => $request->email,
+      'password' => $request->password
+    ];
+
+    $authOK = Auth::guard('admin')->attempt($credentials, $request->remember);
+
+    if($authOK){
+      return redirect()->intended(route('admin.dashboard'));
+    }
+
+    return redirect()->back()->withInputs($request->only('email','remember'));
+
+
+
+  }
+
+  public function index(){
+    return view('auth.login-admin');
+  }
+}
---
+++ loginAvancado/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin', 'AdminController@index')->name('admin.dashboard');
+
+Route::get('/admin/login', 'Auth\AdminLoginController@index')->name('admin.login');
+Route::post('/admin/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+
